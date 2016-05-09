9f715b1e29d9e74fdfeba5e0b5197ca52a17e3b0
Darren <darren.mcnally@gmail.com>
2016-05-09 11:22:32 -0700

    added some code to make scripts work more universally

--- codebook.R
+++ codebook.R
@@ -1,4 +1,4 @@
-if(!file.exists(".UCI HAR Dataset/")) {
+if(!file.exists("./UCI HAR Dataset/")) {
   url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(url, zipped, method = "curl")
   unzip (zipped)}
